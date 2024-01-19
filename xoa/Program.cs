0755870444925d0f24511e9403fd784c840ea4ac
diuancute <dieuantruong1409@gmail.com>
2024-01-19 13:55:07 +0700

    commit2

--- xoa/Program.cs
+++ xoa/Program.cs
@@ -18,6 +18,7 @@ namespace xoa
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
             //comit1
+            //commit2
         }
     }
 }
